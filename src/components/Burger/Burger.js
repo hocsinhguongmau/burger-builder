2ed409d3154c305e970841e5db1418e533c04b8a
thang241190 <thangnguyen24111990@gmail.com>
2019-12-28 00:04:50 +0200

    Add Build Controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,15 +2,27 @@ import React from "react";
 import classes from "./Burger.module.scss";
 import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
 
-const Burger = props => {
+const burger = props => {
+	let transformedIngredients = Object.keys(props.ingredients)
+		.map(igKey => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredients key={igKey + i} type={igKey} />;
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredients!</p>;
+	}
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredients type="bread-top" />
-			<BurgerIngredients type="cheese" />
-			<BurgerIngredients type="meat" />
+			{transformedIngredients}
 			<BurgerIngredients type="bread-bottom" />
 		</div>
 	);
 };
 
-export default Burger;
+export default burger;
